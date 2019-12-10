5d983604246f485db4d88bd7348e5510bbb45f41
Philip Pegion <Philip.Pegion@noaa.gov>
2019-12-10 13:16:12 -0700

    modify confy.py for doxygen

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,3 +57,9 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# create doxygen documentation
+import subprocess
+subprocess.call('cd ../../ ; doxygen', shell=True)
+
+html_extra_path = ['../../html']
