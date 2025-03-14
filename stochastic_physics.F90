00845ccb96ea0fa2cac8e07275e72bce27f0002e
JONG KIM <jong.kim@noaa.gov>
2025-03-14 12:38:52 -0400

    Update stochastic_physics.F90

--- stochastic_physics.F90
+++ stochastic_physics.F90
@@ -164,9 +164,15 @@ if (do_sppt) then
       endif
    enddo
    if (sppt_sfclimit) then
-       do k=1,levs
-       vfact_sppt(k)=pbl_taper(k)
-       enddo
+      if (levs .le. 7) then
+         do k=1,levs
+            vfact_sppt(k)=pbl_taper(k)
+         enddo
+      else
+         do k=1,7
+            vfact_sppt(k)=pbl_taper(k)
+         enddo
+      endif
    endif
    if (is_rootpe()) then
       do k=1,levs
